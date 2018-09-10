3c954d85f152b99321dd48c57ba6a628eff1edf4
Lukasz Wolnik <lukasz@wolnik.co.uk>
2018-09-10 23:35:29 +0100

    Check for food

--- src/util.js
+++ src/util.js
@@ -3,8 +3,11 @@ export const getOutputVenues = (users, venues, filteredUsers) => {
     // get all participating users
     const goingUsers = users.filter(user => filteredUsers.includes(user.name))
 
-    // check for drinks
-    let canGoVenues = venues.filter(venue => goingUsers.reduce((acc, user) => acc && user.drinks.some(drink => venue.drinks.includes(drink)), true))
+    let canGoVenues = venues
+        // check for drinks    
+        .filter(venue => goingUsers.reduce((acc, user) => acc && user.drinks.some(drink => venue.drinks.includes(drink)), true))
+        // check for food
+        .filter(venue => goingUsers.reduce((acc, user) => acc && venue.food.filter(food => !user.wont_eat.includes(food)).length, true))
 
     return {
         canGoVenues,
