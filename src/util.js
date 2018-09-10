c6567d2b0d231e2b393711aaf2bf9b0537a53f5c
Lukasz Wolnik <lukasz@wolnik.co.uk>
2018-09-10 23:22:57 +0100

    Check for drinks

--- src/util.js
+++ src/util.js
@@ -1,5 +1,13 @@
+// return a list of "can go" venues
 export const getOutputVenues = (users, venues, filteredUsers) => {
+    // get all participating users
+    const goingUsers = users.filter(user => filteredUsers.includes(user.name))
+
+    // check for drinks
+    let canGoVenues = venues.filter(venue => goingUsers.reduce((acc, user) => acc && user.drinks.some(drink => venue.drinks.includes(drink)), true))
+
     return {
+        canGoVenues,
         processed: true
     }
 }
