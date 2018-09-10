fedb55ac69b8e8e2a8979bb0457f4f74a15485de
Lukasz Wolnik <lukasz@wolnik.co.uk>
2018-09-11 00:00:40 +0100

    Record any drink/food issues

--- src/util.js
+++ src/util.js
@@ -3,14 +3,41 @@ export const getOutputVenues = (users, venues, filteredUsers) => {
     // get all participating users
     const goingUsers = users.filter(user => filteredUsers.includes(user.name))
 
+    let issues = {}
+
+    for (const venue of venues) {
+        issues[venue.name] = { // for each venue
+            drinks: [], // keep a list of users with missing drinks...
+            food: [] // ...and food
+        }
+    }
+
     let canGoVenues = venues
-        // check for drinks    
-        .filter(venue => goingUsers.reduce((acc, user) => acc && user.drinks.some(drink => venue.drinks.includes(drink)), true))
-        // check for food
-        .filter(venue => goingUsers.reduce((acc, user) => acc && venue.food.filter(food => !user.wont_eat.includes(food)).length, true))
+        // check each venue for drinks    
+        .filter(venue => goingUsers.reduce((acc, user) => {
+            const isDrink = user.drinks.some(drink => venue.drinks.includes(drink))
+
+            if (!isDrink) {
+                // note an issue with missing drinks for the currently checked user
+                issues[venue.name].drinks.push(user.name)
+            }
+
+            return acc && isDrink
+        }, true))
+        // check each remaining venue for food
+        .filter(venue => goingUsers.reduce((acc, user) => {
+            const isFood = venue.food.filter(food => !user.wont_eat.includes(food)).length
+
+            if (!isFood) {
+                // note an issue with food for the currently checked user
+                issues[venue.name].food.push(user.name)
+            }
+
+            return acc && isFood
+        }, true))
 
     return {
         canGoVenues,
-        processed: true
+        issues
     }
 }
